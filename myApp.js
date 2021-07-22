a655f1eacafe5ab895aefe1135ef8aa832746c8a
jj <jjnilton@users.noreply.github.com>
2021-07-22 12:56:21 -0300

    add json response

--- myApp.js
+++ myApp.js
@@ -3,11 +3,11 @@ var app = express();
 console.log("Hello World");
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/views/index.html");
-})
+});
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req, res) => {
-  res.send({message: "Hello World"});
-})
+  res.send({message: "Hello json"});
+});
 
 
 

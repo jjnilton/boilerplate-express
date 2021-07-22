a61c182babceddeef1bec78dcc9dc4292e0e9afc
jj <jjnilton@users.noreply.github.com>
2021-07-22 14:13:01 -0300

    log get requests

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
+app.use((req, res, next) => { 
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+ 	 next();
+});
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/views/index.html");
 });
